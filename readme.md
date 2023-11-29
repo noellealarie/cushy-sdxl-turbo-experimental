b8f2304aa3e9e183179d8d8fff1465fa79232618
noellealarie <146506359+noellealarie@users.noreply.github.com>
2023-11-29 19:51:18 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# SDXL Turbo
+
+[Article on stability.ai](https://stability.ai/news/stability-ai-sdxl-turbo)
+
+[ComfyUI example](https://comfyanonymous.github.io/ComfyUI_examples/sdturbo/)
---
+++ sdxl-turbo.ts
@@ -0,0 +1,62 @@
+import { findParentPreDOMNode } from 'lexical/nodes/LexicalTextNode'
+import { ui_checkpoint, ui_sampler, run_ckpt_vae_clip, run_prompt } from './_prefab'
+
+app({
+    ui: (form) => ({
+        prompt: form.prompt({
+            label: 'Prompt',
+            tooltip: 'SDXL Turbo only uses a positive prompt',
+        }),
+        checkpoint_name: ui_checkpoint(form),
+        sampler_name: ui_sampler(form),
+        steps: form.int({
+            default: 1,
+            label: 'Steps',
+            min: 0,
+            tooltip: 'SDXL Turbo only needs 1 or 2 steps',
+            hideSlider: true,
+        }),
+        seeds: form.seed({
+            label: 'Seed',
+        }),
+        cfg: form.int({
+            default: 1,
+            label: 'CFG',
+            min: 0,
+            tooltip: 'Should be 1',
+            hideSlider: true,
+        }),
+    }),
+    run: async (flow, p) => {
+        const graph = flow.nodes
+        let { model, vae, clip } = run_ckpt_vae_clip(flow, p.checkpoint_name)
+
+        const { prompt, steps, seeds, cfg, sampler_name } = p
+
+        const sigmas = graph.SDTurboScheduler({ model, steps })
+        const sampler = graph.KSamplerSelect({ sampler_name })
+        const latent_image = graph.EmptyLatentImage({
+            width: 512,
+            height: 512,
+            batch_size: 1,
+        })
+
+        const positive = run_prompt(flow, { richPrompt: prompt, clip, ckpt: model, outputWildcardsPicked: true })
+        const negative = graph.CLIPTextEncode({ clip: clip, text: '' })
+
+        const ksampler = graph.SamplerCustom({
+            model,
+            latent_image,
+            positive: positive.conditionning,
+            negative,
+            sampler,
+            sigmas,
+            cfg,
+        })
+
+        graph.SaveImage({
+            filename_prefix: 'sdxl-turbo',
+            images: graph.VAEDecode({ vae, samples: ksampler.outputs.output }),
+        })
+    },
+})
